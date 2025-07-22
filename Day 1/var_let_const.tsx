8c85245886b59ea4aabd4ce571d20ec055b22039
Trần Quang Dũng <tqd0905@gmail.com>
2025-07-23 00:39:07 +0700

    Day 1

---
+++ Day 1/var_let_const.tsx
@@ -0,0 +1,19 @@
+//var cho khai báo lại biến và giá trị có thể thay đổi
+var ageOfDung = 22
+//let không cho khai báo lại biến nhưng giá trị có thể thay đổi
+let nameOfDung = "Dung"
+// const không thể thay đổi giá trị được
+const namSinh = 2003
+
+// namSinh = 2004
+
+// ageOfDung = 23
+// var ageOfDung = 24
+
+// nameOfDung = "CuCuong"
+// let nameOfDung = "Dungdeptraivodich"
+// console.log("Tên: " + nameOfDung + "\n" + "Tuổi: " + ageOfDung + "\n" + "Năm sinh: " + namSinh)
+
+// console.log("Sở thích: " + soThich)
+
+// var string: soThich = "Valorant"
