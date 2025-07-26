a755f253358cb18ec6985dd99bf0083cfdfb1556
Trần Quang Dũng <tqd0905@gmail.com>
2025-07-27 00:18:53 +0700

    Day 5

---
+++ Day 5/cachLayChuoi.tsx
@@ -0,0 +1,8 @@
+let array: number[] = [12,23,43,45]
+// in thì lấy vị trí của mảng , of thì lấy phần tử của mảng
+for(let i in array){
+    console.log(i)
+}
+for(let i of array){
+    console.log(i)
+}
